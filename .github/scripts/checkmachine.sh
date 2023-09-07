1e7732ee7b28de8c4681cd9c1ad5096b0d9d2da4
divya <hello@shortdiv.com>
2023-09-07 20:05:59 +0800

    bleh

--- .github/scripts/checkmachine.sh
+++ .github/scripts/checkmachine.sh
@@ -5,7 +5,7 @@ set -o pipefail
 log=$(
 machine=$( flyctl machine list --json --app "bandsintown" | jq --raw-output 'map(.id)[0]' )
 echo "Attempting to start machine ${machine} for app ..."
-flyctl machine start "${machine}"
+flyctl machine status "${machine}"
 echo ""
 )
 echo -e "log<<EOF\n$log\nEOF" >> $GITHUB_OUTPUT
